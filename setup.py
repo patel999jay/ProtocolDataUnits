13ba07c65f650ae234d38f0c316159761d0e7919
Jay Patel <patel999jay@gmail.com>
2023-10-05 22:06:33 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ProtocolDataUnits",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies project has, e.g.
+        # 'requests',
+    ],
+    author="Jay Patel",
+    author_email="patel999jay@gmail.com",
+    description="ProtocolDataUnits python tools, based on ProtocolDataUnits.jl",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/patel999jay/ProtocolDataUnits",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
