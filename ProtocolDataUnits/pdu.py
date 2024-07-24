1ae5556ac7ce94c04f8079355f9d2045e57d7500
Jay Patel <jy650701@dal.ca>
2024-07-24 12:38:27 -0300

    Update pdu.py
    implemented Fixed-Length Strings, Length-Prefixed Strings & Variable-Length Arrays

--- ProtocolDataUnits/pdu.py
+++ ProtocolDataUnits/pdu.py
@@ -21,13 +21,28 @@ DEFAULT_BYTE_ORDER = '>'
 logging.basicConfig(level=logging.INFO)
 
 
+import struct
+import binascii
+import io
+import logging
+import json
+import zlib
+from loguru import logger
+
+# Constants
+DEFAULT_BYTE_ORDER = '>'
+
+
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO)
+
+
 class PDU:
     def __init__(self):
         self.fields = []
         self.byte_order = DEFAULT_BYTE_ORDER
         self.pdu_length = None
         self.default_values = {}
-        self.aliases = {}
 
     def length(self, length):
         self.pdu_length = length
@@ -98,22 +113,28 @@ class PDU:
             self.default_values[name] = default
         return self
 
-    def string(self, name=None, length=None, default=None):
-        self.fields.append(('string', name, length))
-        if default is not None:
-            self.default_values[name] = default
-        return self
-
     def filler(self, count):
         self.fields.append(('filler', None, count))
         return self
 
     def padding(self, value):
         self.fields.append(('padding', None, value))
+        return self    
+
+    def fixed_string(self, name, length, default=None):
+        self.fields.append(('fixed_string', name, length))
+        if default is not None:
+            self.default_values[name] = default
         return self
 
-    def alias(self, name, alias):
-        self.aliases[alias] = name
+    def length_prefixed_string(self, name, default=None):
+        self.fields.append(('length_prefixed_string', name, None))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+
+    def variable_length_array(self, name, value_type):
+        self.fields.append(('variable_length_array', name, value_type))
         return self
 
     @staticmethod
@@ -127,7 +148,6 @@ class PDU:
         encoded = bytearray()
         for field_type, name, value in self.fields:
             if name is not None:
-                name = self.aliases.get(name, name)  # Resolve alias if any
                 if name not in data and name not in self.default_values:
                     raise ValueError(f"Missing value for field: {name}")
                 value_to_encode = data.get(name, self.default_values.get(name))
@@ -154,8 +174,6 @@ class PDU:
                 encoded_value = struct.pack(self.byte_order + 'f', value_to_encode)
             elif field_type == 'double':
                 encoded_value = struct.pack(self.byte_order + 'd', value_to_encode)
-            elif field_type == 'string':
-                encoded_value = struct.pack(f'{self.byte_order}{value_to_encode}')
             elif field_type == 'filler':
                 encoded_value = b'\x00' * value_to_encode
             elif field_type == 'padding':
@@ -164,7 +182,15 @@ class PDU:
                     encoded_value = bytes([value]) * padding_length
                 else:
                     encoded_value = b''
-            
+            elif field_type == 'fixed_string':
+                encoded_value = struct.pack(f'{self.byte_order}{value}s', value_to_encode.encode('utf-8'))
+            elif field_type == 'length_prefixed_string':
+                encoded_value = struct.pack(self.byte_order + 'I', len(value_to_encode)) + value_to_encode.encode('utf-8')
+            elif field_type == 'variable_length_array':
+                encoded_value = struct.pack(self.byte_order + 'I', len(value_to_encode))
+                for item in value_to_encode:
+                    encoded_value += struct.pack(self.byte_order + {'uint8': 'B', 'int8': 'b', 'uint16': 'H', 'int16': 'h', 'uint32': 'I', 'int32': 'i', 'uint64': 'Q', 'int64': 'q', 'float': 'f', 'double': 'd'}[value], item)
+
             encoded.extend(encoded_value)
         
         # Compute CRC for the data without the CRC field itself
@@ -172,9 +198,9 @@ class PDU:
         crc = self.compute_crc(data_for_crc)
         encoded.extend(struct.pack(self.byte_order + 'I', crc))
 
+        # Compress data if needed
         if compress:
-            encoded = zlib.compress(encoded)
-
+            return zlib.compress(encoded)
         return bytes(encoded)
 
     @logger.catch
@@ -193,21 +219,31 @@ class PDU:
         }
 
         for field_type, name, value in self.fields:
-            if name is None:
-                if field_type in format_map:
-                    offset += struct.calcsize(format_map[field_type])
-                elif field_type == 'filler':
-                    offset += value
-                elif field_type == 'padding':
-                    break
-                continue
-
-            if field_type in format_map:
+            if field_type == 'fixed_string':
+                string_length = value
+                decoded[name] = struct.unpack_from(f'{self.byte_order}{string_length}s', data, offset)[0].decode('utf-8').strip('\x00')
+                offset += struct.calcsize(f'{self.byte_order}{string_length}s')
+            elif field_type == 'length_prefixed_string':
+                string_length = struct.unpack_from(self.byte_order + 'I', data, offset)[0]
+                offset += struct.calcsize(self.byte_order + 'I')
+                decoded[name] = struct.unpack_from(f'{self.byte_order}{string_length}s', data, offset)[0].decode('utf-8')
+                offset += string_length
+            elif field_type == 'variable_length_array':
+                array_length = struct.unpack_from(self.byte_order + 'I', data, offset)[0]
+                offset += struct.calcsize(self.byte_order + 'I')
+                array_items = []
+                for _ in range(array_length):
+                    item, = struct.unpack_from(self.byte_order + format_map[value], data, offset)
+                    array_items.append(item)
+                    offset += struct.calcsize(self.byte_order + format_map[value])
+                decoded[name] = array_items
+            elif field_type in format_map:
                 decoded[name], = struct.unpack_from(self.byte_order + format_map[field_type], data, offset)
                 offset += struct.calcsize(format_map[field_type])
-            elif field_type == 'string':
-                decoded[name] = data[offset:offset + value].decode()
+            elif field_type == 'filler':
                 offset += value
+            elif field_type == 'padding':
+                break
 
         # Extract the CRC from the end of the data
         crc_expected = struct.unpack_from(self.byte_order + 'I', data, self.pdu_length - 4)[0]
@@ -236,8 +272,7 @@ class PDU:
             else:
                 pdu = getattr(pdu, field[0])(*field[1:])
         return pdu
-
-
+    
 def create_pdu_format(length, byte_order, *fields):
     pdu = PDU().length(length).order(byte_order)
     for field in fields:
@@ -245,12 +280,68 @@ def create_pdu_format(length, byte_order, *fields):
     return pdu
 
 # Usage example
+# if __name__ == "__main__":
+#     # Example for dynamic PDU format definition
+#     my_pdu = PDU().length(24).order('big').uint8('type').float('value1').double('value2').padding(0xff)
+
+#     # Encoding and decoding
+#     encoded_bytes = my_pdu.encode({'type': 7, 'value1': 3.14, 'value2': 6.28}, compress=True)
+#     print(f"Encoded Bytes: {encoded_bytes}")
+
+#     decoded_data = my_pdu.decode(encoded_bytes, decompress=True)
+#     print(f"Decoded Data: {decoded_data}")
+    
+#     # Serialization to JSON
+#     json_str = my_pdu.to_json()
+#     print(f"Serialized PDU to JSON:   {json_str}")
+
+#     # Deserialization from JSON
+#     new_pdu = PDU.from_json(json_str)
+#     print(f"Deserialized PDU from JSON: {new_pdu.to_json()}")
+    
+#     # Encoding and decoding using deserialized PDU
+#     encoded_bytes_new = new_pdu.encode({'type': 7, 'value1': 3.14, 'value2': 6.28}, compress=True)
+#     print(f"Encoded Bytes (new PDU): {encoded_bytes_new}")
+
+#     decoded_data_new = new_pdu.decode(encoded_bytes_new, decompress=True)
+#     print(f"Decoded Data (new PDU): {decoded_data_new}")
+
+#     """
+#     INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
+#     Encoded Bytes: b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\xff\x10p\xe7\xd0\x83g\x00\x9d\xf7\x0f\xb4'
+#     INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
+#     Decoded Data: {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28}
+#     Serialized PDU to JSON:   {"length": 24, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["padding", null, 255]]}
+#     Deserialized PDU from JSON: {"length": 24, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["padding", null, 255]]}
+#     INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
+#     Encoded Bytes (new PDU): b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\xff\x10p\xe7\xd0\x83g\x00\x9d\xf7\x0f\xb4'
+#     INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
+#     Decoded Data (new PDU): {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28}
+#     """
+
+
+# Usage example
 if __name__ == "__main__":
-    # Example for dynamic PDU format definition
-    my_pdu = PDU().length(24).order('big').uint8('type').float('value1').double('value2').padding(0xff)
+    # Example for dynamic PDU format definition with various types of fields
+    my_pdu = PDU().length(64).order('big') \
+        .uint8('type') \
+        .float('value1') \
+        .double('value2') \
+        .fixed_string('fixed_str', 10, default="default") \
+        .length_prefixed_string('length_str', default="") \
+        .variable_length_array('array', 'uint8') \
+        .padding(0xff)
 
     # Encoding and decoding
-    encoded_bytes = my_pdu.encode({'type': 7, 'value1': 3.14, 'value2': 6.28}, compress=True)
+    data_to_encode = {
+        'type': 7,
+        'value1': 3.14,
+        'value2': 6.28,
+        'fixed_str': "hello",
+        'length_str': "dynamic string",
+        'array': [1, 2, 3, 4, 5]
+    }
+    encoded_bytes = my_pdu.encode(data_to_encode, compress=True)
     print(f"Encoded Bytes: {encoded_bytes}")
 
     decoded_data = my_pdu.decode(encoded_bytes, decompress=True)
@@ -258,28 +349,28 @@ if __name__ == "__main__":
 
     # Serialization to JSON
     json_str = my_pdu.to_json()
-    print(f"Serialized PDU to JSON:   {json_str}")
+    print(f"Serialized PDU to JSON: {json_str}")
 
     # Deserialization from JSON
     new_pdu = PDU.from_json(json_str)
     print(f"Deserialized PDU from JSON: {new_pdu.to_json()}")
 
     # Encoding and decoding using deserialized PDU
-    encoded_bytes_new = new_pdu.encode({'type': 7, 'value1': 3.14, 'value2': 6.28}, compress=True)
+    encoded_bytes_new = new_pdu.encode(data_to_encode, compress=True)
     print(f"Encoded Bytes (new PDU): {encoded_bytes_new}")
 
     decoded_data_new = new_pdu.decode(encoded_bytes_new, decompress=True)
     print(f"Decoded Data (new PDU): {decoded_data_new}")
 
     """
-    INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
-    Encoded Bytes: b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\xff\x10p\xe7\xd0\x83g\x00\x9d\xf7\x0f\xb4'
-    INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
-    Decoded Data: {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28}
-    Serialized PDU to JSON:   {"length": 24, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["padding", null, 255]]}
-    Deserialized PDU from JSON: {"length": 24, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["padding", null, 255]]}
-    INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
-    Encoded Bytes (new PDU): b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\xff\x10p\xe7\xd0\x83g\x00\x9d\xf7\x0f\xb4'
-    INFO:root:Data for CRC: 074048f5c340191eb851eb851fffffffffffffff, CRC: 3703759078
-    Decoded Data (new PDU): {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28}
+    INFO:root:Data for CRC: 074048f5c340191eb851eb851f68656c6c6f00000000000000000e64796e616d696320737472696e67000000050102030405ffffffffffffffffffff, CRC: 3954678632
+    Encoded Bytes: b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\x8c\xd4\x9c\x9c|\x06(\xe0K\xa9\xccK\xcc\xcdLV(.)\xca\xccK\x07\x8a\xb0221\xb3\xb0\xfe\x87\x83\xd7\xdbgg\x00\x00\xad\xe0\x19\xc4'
+    INFO:root:Data for CRC: 074048f5c340191eb851eb851f68656c6c6f00000000000000000e64796e616d696320737472696e67000000050102030405ffffffffffffffffffff, CRC: 3954678632
+    Decoded Data: {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28, 'fixed_str': 'hello', 'length_str': 'dynamic string', 'array': [1, 2, 3, 4, 5]}
+    Serialized PDU to JSON: {"length": 64, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["fixed_string", "fixed_str", 10], ["length_prefixed_string", "length_str", null], ["variable_length_array", "array", "uint8"], ["padding", null, 255]]}
+    Deserialized PDU from JSON: {"length": 64, "byte_order": "big", "fields": [["uint8", "type", null], ["float", "value1", null], ["double", "value2", null], ["fixed_string", "fixed_str", 10], ["length_prefixed_string", "length_str", null], ["variable_length_array", "array", "uint8"], ["padding", null, 255]]}
+    INFO:root:Data for CRC: 074048f5c340191eb851eb851f68656c6c6f00000000000000000e64796e616d696320737472696e67000000050102030405ffffffffffffffffffff, CRC: 3954678632
+    Encoded Bytes (new PDU): b'x\x9ccw\xf0\xf8z\xd8ARnG\xe0\xebV\xf9\x8c\xd4\x9c\x9c|\x06(\xe0K\xa9\xccK\xcc\xcdLV(.)\xca\xccK\x07\x8a\xb0221\xb3\xb0\xfe\x87\x83\xd7\xdbgg\x00\x00\xad\xe0\x19\xc4'
+    INFO:root:Data for CRC: 074048f5c340191eb851eb851f68656c6c6f00000000000000000e64796e616d696320737472696e67000000050102030405ffffffffffffffffffff, CRC: 3954678632
+    Decoded Data (new PDU): {'type': 7, 'value1': 3.140000104904175, 'value2': 6.28, 'fixed_str': 'hello', 'length_str': 'dynamic string', 'array': [1, 2, 3, 4, 5]}
     """
