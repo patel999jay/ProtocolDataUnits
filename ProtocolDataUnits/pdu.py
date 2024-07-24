8785dad5ccb87158a4d068ef543d72cddef7702c
Jay Patel <jy650701@dal.ca>
2024-07-24 11:52:41 -0300

    Update pdu.py
    added support for more Data Types

--- ProtocolDataUnits/pdu.py
+++ ProtocolDataUnits/pdu.py
@@ -9,7 +9,8 @@ import struct
 import binascii
 import io
 import logging
-from loguru import logger # pip install loguru
+from loguru import logger # as logging
+
 # Constants
 DEFAULT_BYTE_ORDER = '>'
 
@@ -23,6 +24,7 @@ class PDU:
         self.fields = []
         self.byte_order = DEFAULT_BYTE_ORDER
         self.pdu_length = None
+        self.default_values = {}
 
     def length(self, length):
         self.pdu_length = length
@@ -32,24 +34,65 @@ class PDU:
         self.byte_order = '>' if byte_order == 'big' else '<'
         return self
 
-    def uint8(self, name=None, value=None):
+    def uint8(self, name=None, value=None, default=None):
         self.fields.append(('uint8', name, value))
+        if default is not None:
+            self.default_values[name] = default
         return self
 
-    def uint16(self, name=None, value=None):
+    def uint16(self, name=None, value=None, default=None):
         self.fields.append(('uint16', name, value))
+        if default is not None:
+            self.default_values[name] = default
         return self
-
-    def uint32(self, name=None, value=None):
+    
+    def uint32(self, name=None, value=None, default=None):
         self.fields.append(('uint32', name, value))
+        if default is not None:
+            self.default_values[name] = default
         return self
 
-    def float(self, name=None, value=None):
+    def float(self, name=None, value=None, default=None):
         self.fields.append(('float', name, value))
+        if default is not None:
+            self.default_values[name] = default
         return self
 
-    def double(self, name=None, value=None):
+    def double(self, name=None, value=None, default=None):
         self.fields.append(('double', name, value))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+    
+    # Additional methods for other types
+    def int8(self, name=None, value=None, default=None):
+        self.fields.append(('int8', name, value))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+
+    def int16(self, name=None, value=None, default=None):
+        self.fields.append(('int16', name, value))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+
+    def int32(self, name=None, value=None, default=None):
+        self.fields.append(('int32', name, value))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+
+    def int64(self, name=None, value=None, default=None):
+        self.fields.append(('int64', name, value))
+        if default is not None:
+            self.default_values[name] = default
+        return self
+
+    def uint64(self, name=None, value=None, default=None):
+        self.fields.append(('uint64', name, value))
+        if default is not None:
+            self.default_values[name] = default
         return self
 
     def filler(self, count):
@@ -58,7 +101,7 @@ class PDU:
 
     def padding(self, value):
         self.fields.append(('padding', None, value))
-        return self
+        return self    
 
     @staticmethod
     def compute_crc(data):
@@ -71,18 +114,28 @@ class PDU:
         encoded = bytearray()
         for field_type, name, value in self.fields:
             if name is not None:
-                if name not in data:
+                if name not in data and name not in self.default_values:
                     raise ValueError(f"Missing value for field: {name}")
-                value_to_encode = data[name]
+                value_to_encode = data.get(name, self.default_values.get(name))
             else:
                 value_to_encode = value
 
             if field_type == 'uint8':
                 encoded_value = struct.pack(self.byte_order + 'B', value_to_encode)
+            elif field_type == 'int8':
+                encoded_value = struct.pack(self.byte_order + 'b', value_to_encode)
             elif field_type == 'uint16':
                 encoded_value = struct.pack(self.byte_order + 'H', value_to_encode)
+            elif field_type == 'int16':
+                encoded_value = struct.pack(self.byte_order + 'h', value_to_encode)
             elif field_type == 'uint32':
                 encoded_value = struct.pack(self.byte_order + 'I', value_to_encode)
+            elif field_type == 'int32':
+                encoded_value = struct.pack(self.byte_order + 'i', value_to_encode)
+            elif field_type == 'uint64':
+                encoded_value = struct.pack(self.byte_order + 'Q', value_to_encode)
+            elif field_type == 'int64':
+                encoded_value = struct.pack(self.byte_order + 'q', value_to_encode)
             elif field_type == 'float':
                 encoded_value = struct.pack(self.byte_order + 'f', value_to_encode)
             elif field_type == 'double':
@@ -104,10 +157,17 @@ class PDU:
         encoded.extend(struct.pack(self.byte_order + 'I', crc))
         return bytes(encoded)
 
+    @logger.catch
     def decode(self, data):
         decoded = {}
         offset = 0
-        format_map = {'uint8': 'B', 'uint16': 'H', 'uint32': 'I', 'float': 'f', 'double': 'd'}
+        format_map = {
+            'uint8': 'B', 'int8': 'b',
+            'uint16': 'H', 'int16': 'h',
+            'uint32': 'I', 'int32': 'i',
+            'uint64': 'Q', 'int64': 'q',
+            'float': 'f', 'double': 'd'
+        }
 
         for field_type, name, value in self.fields:
             if name is None:
@@ -132,7 +192,7 @@ class PDU:
             raise ValueError(f"CRC mismatch: computed={crc_computed}, expected={crc_expected}")
 
         return decoded
-    
+
 def create_pdu_format(length, byte_order, *fields):
     pdu = PDU().length(length).order(byte_order)
     for field in fields:
@@ -150,3 +210,4 @@ if __name__ == "__main__":
 
     decoded_data = my_pdu.decode(encoded_bytes)
     print(f"Decoded Data: {decoded_data}")
+
